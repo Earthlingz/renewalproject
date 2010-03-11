02bb8054d15f85714765697233a5583746466240
p0k3m0nt3am <p0k3m0nt3am@0dfc2db8-21ea-11df-af09-616cd6757146>
2010-03-11 15:59:47 +0000

    rev11 - [2010-03-11]  - Translated Optioninfo.lua to English  - Updated and Translated party_booking.lua to English but still incomplete

--- lua files/seekparty/party_booking.lua
+++ lua files/seekparty/party_booking.lua
@@ -57,65 +57,249 @@ map_list = {
 	{name = "Turtle Island Dungeon"},
 	{name = "Good Turtles Village"},
 	{name = "Turtle Palace", colorR = 255, colorG = 0, colorB = 0}}
-	}
+	},
+	{list = {name = "Morocc",
+	{name = "Sograt Desert", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Morocc Field (Dimensional Gorge)", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Ant Hell F1"},
+	{name = "Ant Hell F2", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Sphinx B1"},
+	{name = "Sphinx B2"},
+	{name = "Sphinx B3"},
+	{name = "Sphinx B4"},
+	{name = "Sphinx B5", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Inside Pyramid F1"},
+	{name = "Inside Pyramid F2"},
+	{name = "Inside Pyramid F3"},
+	{name = "Inside Pyramid F4", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Inside Pyramid B1"},
+	{name = "Inside Pyramid B2", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Izlude",
+	{name = "Undersea Tunnel F1"},
+	{name = "Undersea Tunnel F2"},
+	{name = "Undersea Tunnel F3"},
+	{name = "Undersea Tunnel F4"},
+	{name = "Undersea Tunnel F5"}}
+	},
+	{list = {name = "Comodo",
+	{name = "Papuchicha Forest"},
+	{name = "Kokomo Beach"},
+	{name = "Zenhai Marsh"},
+	{name = "Pharos Beacon Island"},
+	{name = "Fortress Saint Darmain (East)"},
+	{name = "Fortress Saint Darmain (South)"},
+	{name = "Fortress Saint Darmain (West)"},
+	{name = "Karu, the West Cave", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Ruande, the North Cave"},
+	{name = "Mao, the East Cave"}}
+	},
+	{list = {name = "Lutie",
+	{name = "Field", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Toy Factory Warehouse"},
+	{name = "Toy Monitoring Room", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Yuno",
+	{name = "Border Checkpoint/Post"},
+	{name = "Kiel Hyre's Cottage"},
+	{name = "El Mes Plateau"},
+	{name = "El Mes Gorge (Valley of Abyss)"},
+	{name = "Kiel Hyre's Academy"},
+	{name = "Schwarzwald Guards Camp"},
+	{name = "Field"},
+	{name = "Nogg Road F1"},
+	{name = "Nogg Road F2"},
+	{name = "External Juperus Ruins"},
+	{name = "Inside Juperus Ruins"},
+	{name = "Center of Juperos", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Robot Factory Level F1"},
+	{name = "Robot Factory Level F2", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Umbala",
+	{name = "Luluka Forest"},
+	{name = "Kalala Swamp"},
+	{name = "Hoomga Forest"},
+	{name = "Hoomga Jungle"}}
+	},
+	{list = {name = "Einbech",
+	{name = "Mine Dungeon F1"},
+	{name = "Mine Dungeon F2", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Lighthalzen",
+	{name = "Field"},
+	{name = "Lighthalzen Field (Grim Reaper's Valley)"},
+	{name = "Somatology Laboratory F1"},
+	{name = "Somatology Laboratory F2", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Somatology Laboratory F3", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Thanatos Tower",
+	{name = "Hacheungbu Museum Entrance"}, --need translator please :)
+	{name = "Hacheungbu Museum"},
+	{name = "Hacheungbu Abandoned Space"},
+	{name = "Upper Level"},
+	{name = "Upper Room of Angel"},
+	{name = "Upper Room of Agony"},
+	{name = "Upper Room of Sorrow"},
+	{name = "Upper Room of Despair"},
+	{name = "Upper Room of Hatred"},
+	{name = "Unknown Area", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Glast Heim",
+	{name = "Glast Heim F1"},
+	{name = "Glast Heim F2"},
+	{name = "St. Abbey"},
+	{name = "Catacombs", colorR = 255, colorG = 0, colorB = 0}, --Churchyard[English official server]
+	{name = "Underground B1"}, --The Lowest Cave in Glast Heim F1[English official server]
+	{name = "Underground B2"}, --The Lowest Cave in Glast Heim F2[English official server]
+	{name = "Inside"}, --Inside Glast Heim[English official server]
+	{name = "Chivalry F1"},
+	{name = "Chivalry F2"},
+	{name = "Underprison F1"},
+	{name = "Underprison F2"},
+	{name = "Culvert F1"},
+	{name = "Culvert F2"},
+	{name = "Culvert F3"},
+	{name = "Culvert F4"},
+	{name = "Staircase Dungeon"}}
+	},
+	{list = {name ="Hugel",
+	{name = "Field"},
+	{name = "Odin Temple West"},
+	{name = "Odin Temple South"},
+	{name = "Odin Temple North", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Abyss Lake"},
+	{name = "Abyss Lake Underground Cave F1"},
+	{name = "Abyss Lake Underground Cave F2"},
+	{name = "Abyss Lake Underground Cave F3", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Infront of Thanatos Tower"}} --What?
+	},
+	{list = {name = "Rachel",
+	{name = "Audumra Grass Land"},
+	{name = "Ida Plane", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Fortu Luna", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Temple Sanctuary North F1"}, --Freya's Sacred Precinct F1
+	{name = "Temple Sanctuary West F1"}, --Freya's Sacred Precinct F2
+	{name = "Temple Sanctuary East F1"}, --Freya's Sacred Precinct F3
+	{name = "Temple Sanctuary South F1"}, --Freya's Sacred Precinct F4
+	{name = "Temple Sanctuary Center", colorR = 255, colorG = 0, colorB = 0}, --Freya's Sacred Precinct F5
+	{name = "Ice Cave F1"},
+	{name = "Ice Cave F2"},
+	{name = "Ice Cave F3"}}
+	},
+	{list = {name = "Veins",
+	{name = "Field", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Nameless Island"},
+	{name = "Cursed Abbey F1"},
+	{name = "Cursed Abbey B1", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Cursed Abbey B2", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Thor's Volcano Dungeon F1"},
+	{name = "Thor's Volcano Dungeon F2"},
+	{name = "Thor's Volcano Dungeon F3", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Niflheim",
+	{name = "Skellington Solitary Village"},
+	{name = "Valley of Gyoll"},
+	{name = "Niflheim", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Passage", -- huh?
+	{name = "Manuk Field"},
+	{name = "Splendide Field"},
+	{name = "Foot of Kamidal Mountain"}, --El Descates Field Foot Of The Kamidal Mountain
+	{name = "Kamidal Tunnel"}, --Underground Passage Kamidal Tunnel
+	{name = "Scaraba Hole", colorR = 255, colorG = 0, colorB = 0}, --Underground Nest Scaraba Hole
+	{name = "Nidhogg's Dungeon F1"},
+	{name = "Nidhogg's Dungeon F2"}}
+	},
+	{list = {name = "Amatsu",
+	{name = "Field"},
+	{name = "Tatami Maze"},
+	{name = "Underground Forest Battle Field"},
+	{name = "Underground Shrine", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Gonryun",
+	{name = "Field" },
+	{name = "Queen's Shrine" },
+	{name = "Hermit's Checkers" },
+	{name = "Arcadia", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Louyang",
+	{name = "Field"},
+	{name = "Royal Tomb"},
+	{name = "Inside the Royal Tomb"},
+	{name = "Suei Long Gon", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Ayothaya",
+	{name = "Field"},
+	{name = "Ancient Shrine Maze"},
+	{name = "Inside Ancient Shrine", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Brasilis", -- or Brasilia?
+	{name = "Field"},
+	{name = "Waterfall Cave Entrance"},  --Brasilia's Dungeon F1
+	{name = "Inside Waterfall Cave", colorR = 255, colorG = 0, colorB = 0}}  --Brasilia's Dungeon F2
+	},
+	{list = {name = "Moscovia",
+	{name = "Field"}, --Okrestnosti Of Moscovia
+	{name = "Les Forest"},
+	{name = "Temny Forest"},
+	{name = "Dremuci Forest", colorR = 255, colorG = 0, colorB = 0}}
+	},
+	{list = {name = "Memorial Dungeon",
+	{name = "Endless Tower", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Sealed Shrine", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Orc Memories", colorR = 255, colorG = 0, colorB = 0},
+	{name = "Nidhogg's Nest", colorR = 255, colorG = 0, colorB = 0}}
+	},
 }
---- Incomplete working on it.
+printMapList = function() 
+	local regionId = 0
+	local mapId = 0
+	local state = ""
+	print("------------------")
+	for key, region in pairs(map_list) do
+		regionId = regionId +1
+		mapId = 0 
+		if region["ignore_recruit_window"] == true then
+			state = "파티 모집 (신청) 창에서는 보이지 않는 메뉴임"
+		else
+			state = ""
+		end
+		print(regionId,region["name"],state)
+		for key, map in pairs(region["list"]) do
+			mapId = mapId +1
+			if map["color"] == nil then
+				color = "기본색"
+			else 
+				color = "R: " .. map.color.R .. "G:" .. map.color.G .. "B:" .. map.color.B
+			end
+			print("\t" .. mapId .. ":" .. map.name, map.map_file, color)
+		end
+	end
+	print("------------------")
+end
 
+__mapList = {}
+makeMapList = function()
+	for regionId, region in ipairs(map_list) do
+		__mapList[regionId] = region
+		region.id = regionId
+		for mapId, map in ipairs(region["list"]) do
+			__mapList[regionId] = map[mapId] -- cant get this one to have its proper structure line reference in 302 to 304
+			map.id = mapId
+			map.ignore_recruit_window = region["ignore_recruit_window"]
+	end
+	end
+end
 
 --[[
-map_list = { Next Batch = Morocc
-2DBF  8A000007           [154] newtable   7   0   2    ; array=0, hash=4
-2DC3  09CD7D07           [155] settable   7   251 308  ; "name" "모로크"
-2DC7  0A000A08           [156] newtable   8   20  0    ; array=16, hash=0
-2DCB  CA000009           [157] newtable   9   0   3    ; array=0, hash=8
-2DCF  49CD7D09           [158] settable   9   251 309  ; "name" "소그라트 사막"
-2DD3  89C18209           [159] settable   9   261 262  ; "colorR" 255
-2DD7  09C28309           [160] settable   9   263 264  ; "colorG" 0
-2DDB  09C28409           [161] settable   9   265 264  ; "colorB" 0
-2DDF  CA00000A           [162] newtable   10  0   3    ; array=0, hash=8
-2DE3  89CD7D0A           [163] settable   10  251 310  ; "name" "소그라트 사막 차원의 균열"
-2DE7  89C1820A           [164] settable   10  261 262  ; "colorR" 255
-2DEB  09C2830A           [165] settable   10  263 264  ; "colorG" 0
-2DEF  09C2840A           [166] settable   10  265 264  ; "colorB" 0
-2DF3  4A00000B           [167] newtable   11  0   1    ; array=0, hash=2
-2DF7  C9CD7D0B           [168] settable   11  251 311  ; "name" "개미지옥 던전 1층"
-2DFB  CA00000C           [169] newtable   12  0   3    ; array=0, hash=8
-2DFF  09CE7D0C           [170] settable   12  251 312  ; "name" "개미지옥 던전 2층"
-2E03  89C1820C           [171] settable   12  261 262  ; "colorR" 255
-2E07  09C2830C           [172] settable   12  263 264  ; "colorG" 0
-2E0B  09C2840C           [173] settable   12  265 264  ; "colorB" 0
-2E0F  4A00000D           [174] newtable   13  0   1    ; array=0, hash=2
-2E13  49CE7D0D           [175] settable   13  251 313  ; "name" "스핑크스 지하 1층"
-2E17  4A00000E           [176] newtable   14  0   1    ; array=0, hash=2
-2E1B  89CE7D0E           [177] settable   14  251 314  ; "name" "스핑크스 지하 2층"
-2E1F  4A00000F           [178] newtable   15  0   1    ; array=0, hash=2
-2E23  C9CE7D0F           [179] settable   15  251 315  ; "name" "스핑크스 지하 3층"
-2E27  4A000010           [180] newtable   16  0   1    ; array=0, hash=2
-2E2B  09CF7D10           [181] settable   16  251 316  ; "name" "스핑크스 지하 4층"
-2E2F  CA000011           [182] newtable   17  0   3    ; array=0, hash=8
-2E33  49CF7D11           [183] settable   17  251 317  ; "name" "스핑크스 지하 5층"
-2E37  89C18211           [184] settable   17  261 262  ; "colorR" 255
-2E3B  09C28311           [185] settable   17  263 264  ; "colorG" 0
-2E3F  09C28411           [186] settable   17  265 264  ; "colorB" 0
-2E43  4A000012           [187] newtable   18  0   1    ; array=0, hash=2
-2E47  89CF7D12           [188] settable   18  251 318  ; "name" "피라미드 지상 1층"
-2E4B  4A000013           [189] newtable   19  0   1    ; array=0, hash=2
-2E4F  C9CF7D13           [190] settable   19  251 319  ; "name" "피라미드 지상 2층"
-2E53  4A000014           [191] newtable   20  0   1    ; array=0, hash=2
-2E57  09D07D14           [192] settable   20  251 320  ; "name" "피라미드 지상 3층"
-2E5B  CA000015           [193] newtable   21  0   3    ; array=0, hash=8
-2E5F  49D07D15           [194] settable   21  251 321  ; "name" "피라미드 지상 4층"
-2E63  89C18215           [195] settable   21  261 262  ; "colorR" 255
-2E67  09C28315           [196] settable   21  263 264  ; "colorG" 0
-2E6B  09C28415           [197] settable   21  265 264  ; "colorB" 0
-2E6F  4A000016           [198] newtable   22  0   1    ; array=0, hash=2
-2E73  89D07D16           [199] settable   22  251 322  ; "name" "피라미드 지하 1층"
-2E77  CA000017           [200] newtable   23  0   3    ; array=0, hash=8
-2E7B  C9D07D17           [201] settable   23  251 323  ; "name" "피라미드 지하 2층"
-2E7F  89C18217           [202] settable   23  261 262  ; "colorR" 255
-2E83  09C28317           [203] settable   23  263 264  ; "colorG" 0
-2E87  09C28417           [204] settable   23  265 264  ; "colorB" 0
-2E8B  9F030008           [205] setlist    8   14       ; index 1 to 15
-2E8F  09827E07           [206] settable   7   253 8    ; "list"
- }
+makeMapList()
+print["All Output Data Structure"]
+getRegionIterator()
+
+Incomplete working on it.
+5 Functions left and it's done.
+
+249C  85000008           [12] getglobal  8   2        ; __mapList
+24A0  86000408           [13] gettable   8   8   2  
+24A4  C9010308           [14] settable   8   6   7  
 --]]
